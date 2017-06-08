be199b37afa0a27904e614cf7b22fab03e8bfdff
Matt <matthew.smith@dal.ca>
2017-06-08 12:01:56 -0300

    Some unit tests - mb

---
+++ app/src/test/java/com/example/greenglobal/greenhousemonitor/UnitTest.java
@@ -0,0 +1,30 @@
+package com.example.greenglobal.greenhousemonitor;
+
+/**
+ * Created by Matt on 2017-06-08.
+ */
+
+import org.androidannotations.rest.spring.annotations.Accept;
+import org.junit.Test;
+
+import static junit.framework.Assert.*;
+
+public class UnitTest {
+
+    @Test
+    public void lightLevelsIsNotNull() throws Exception{
+        double lightLevel = 10;
+
+        assertNotNull(lightLevel);
+    }
+
+    @Test
+    public void humidityLevelsIsNotNull() throws Exception {
+        double humidityLevel = 10;
+
+        assertNotNull(humidityLevel);
+    }
+
+
+
+}
